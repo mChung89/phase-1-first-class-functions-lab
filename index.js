2e8c87b1469efca8e5b45e38b9bcced6745f051d
Michael Chung <mikechung89@gmail.com>
2022-02-23 01:54:16 -0500

    commit tree 9c51fd1293b229abc8914480d1df638ebda3ab53

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers(arr) {
+    const drivers = arr.slice(0,2);
+    return drivers
+}
+
+function returnLastTwoDrivers(arr) {
+    const drivers = arr.slice(-2);
+    return drivers
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(int) {
+    return function(fare) {
+        return fare * int
+    }
+}
+
+function fareDoubler(fare) {
+    const doubler = createFareMultiplier(2)(fare);
+    return doubler
+}
+
+function fareTripler(fare) {
+    const tripler = createFareMultiplier(3)(fare);
+    return tripler
+}
+
+function selectDifferentDrivers(arrayOfDrivers, either) {
+    let select = either(arrayOfDrivers);
+    return select
+}
